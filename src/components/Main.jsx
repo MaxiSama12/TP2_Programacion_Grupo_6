bc3dac436b559a26645d09b1e0131a3710adea15
GabrielTimo006 <gabitimo006@gmail.com>
2025-04-26 12:46:24 -0300

    Implementar el estado y el manejador de cambios en los inputs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,16 +12,53 @@ function Main({ integrantes }) {
     foto: ''
   });
 
+  const handleChange = (e) => {
+    setNuevo({
+      ...nuevo,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
     <div className="container">
       <h2>Agregar integrante</h2>
 
       <form>
-        <input type="text" name="nombre" placeholder="Nombre" />
-        <input type="text" name="apellido" placeholder="Apellido" />
-        <input type="text" name="legajo" placeholder="Legajo" />
-        <input type="text" name="github" placeholder="GitHub" />
-        <input type="text" name="foto" placeholder="URL de foto" />
+        <input
+          type="text"
+          name="nombre"
+          placeholder="Nombre"
+          value={nuevo.nombre}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="apellido"
+          placeholder="Apellido"
+          value={nuevo.apellido}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="legajo"
+          placeholder="Legajo"
+          value={nuevo.legajo}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="github"
+          placeholder="GitHub"
+          value={nuevo.github}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="foto"
+          placeholder="URL de foto"
+          value={nuevo.foto}
+          onChange={handleChange}
+        />
         <button type="submit">Agregar</button>
       </form>
 
