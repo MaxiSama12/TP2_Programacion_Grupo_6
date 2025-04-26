c46d7cae72a09430f38f3638ce6b8e465e61eaaf
Franco Giacobbe <frandg47@gmail.com>
2025-04-25 21:12:59 -0300

    simulando envios de props

--- src/components/CardNosotros.jsx
+++ src/components/CardNosotros.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { FaGithub } from "react-icons/fa";
+import { FaUser } from "react-icons/fa6";
 
-const CardNosotros = () => {
+const CardNosotros = ({nombre, github, img}) => {
   return (
     <>
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={img} />
         <Card.Body className="d-grid">
-          <Card.Title>Franco Giacobbe</Card.Title>
+          <Card.Title>{nombre}</Card.Title>
           <Card.Text>Legajo: 48600</Card.Text>
-          <a href="/" className="text-reset w-100 text-decoration-none d-flex align-items-center justify-content-center text-center">
+          <a href={github} className="text-reset w-100 text-decoration-none d-flex align-items-center justify-content-center text-center">
             <Button
               variant="outline-dark"
               className="w-100 mb-2 d-flex align-items-center justify-content-center text-center"
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,29 +1,21 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import CardNosotros from "./CardNosotros";
 
 const Nosotros = () => {
+
+  const usuarios = [{
+    nombre: "franco",
+    github: "github",
+    img: "img"
+  }];
+
   return (
     <div className="container">
       <div className="row my-5">
         <div className="col-4 mb-5">
-          <CardNosotros />
-        </div>
-        <div className="col-4 mb-5">
-          <CardNosotros />
-        </div>
-        <div className="col-4 mb-5">
-          <CardNosotros />
-        </div>
-        <div className="col-4 mb-5">
-          <CardNosotros />
-        </div>
-        <div className="col-4 mb-5">
-          <CardNosotros />
-        </div>
-        <div className="col-4 mb-5">
-          <CardNosotros />
+          {usuarios.map(({nombre, github, img}, key) => (
+            <CardNosotros key={key} nombre={nombre} github={github} img={img} />
+          ))}
         </div>
       </div>
     </div>
