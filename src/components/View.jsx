4b0a1f4404acbd5489e95c9251e7abbe5f14894c
PbloRuiz <pruiz2864@gmail.com>
2025-04-26 10:12:35 -0300

    creé el componente View.jsx para mostrar detalle de cada integrante

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import {useLocation} from 'react-router-dom';
+import './styles/View.css';
 
-const View = () => {
+
+function View() {
+  const location = useLocation();
+  const integrante = location.state; // Contiene el objeto del integrante que fue pasado desde otra página
+  
+  
   return (
-    <div>View</div>
-  )
+    <div className="container text-center py-5">
+      <h2 className="mb-4">{integrante.nombre} {integrante.apellido}</h2>
+
+      <img 
+      src={integrante.foto}
+      alt={`${integrante.nombre} ${integrante.apellido}`}
+      className="rounded-circle mb-4"
+      width="150"
+      height="150"
+      style={{objectFit: 'cover'}}
+      />
+
+      <p><strong>Legajo:</strong>{integrante.legajo}</p>
+      <p>
+        <a 
+        href={integrante.github}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn btn-outline-info mt-3"
+        >Ver GitHub</a>
+      </p>
+    </div>
+  );
 }
 
-export default View
+export default View;
