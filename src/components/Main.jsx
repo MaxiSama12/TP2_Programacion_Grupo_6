1a9526ca41b00756ac7d87ac6d77c8f3746d7421
Micaela <luzmicaelaponce00@gmail.com>
2025-04-25 21:55:28 -0300

    Agrego renderizado dinámico de integrantes en Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Main = () => {
+const Main = ({integrantes}) => {
   return (
-    <div>Main</div>
+    <div>
+      <h1>Integrantes del grupo</h1>
+      {integrantes.map((integrante, index) => (
+        <div key={index}>
+          <h2>{integrante.nombre} {integrante.apellido}</h2>
+          <p>Legajo: {integrante.legajo}</p>
+          <a href={integrante.github} target="_blank" rel="noopener noreferrer">GitHub</a>
+          <img src={integrante.foto} alt={`${integrante.nombre} ${integrante.apellido}`} />
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Main
+export default Main;
